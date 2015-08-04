a6d4d220abfa32bc206677ff26e06e310299381a
Steffen Tiedemann Christensen <steffen@refresh.dk>
2015-08-04 15:41:30 +0200

    Update JavaScript with new classes in Github issues

--- things-for-github.js
+++ things-for-github.js
@@ -3,12 +3,12 @@
   if (!document.getElementsByClassName("js-comment-edit-button").length)
     return;
 
-  var issueTitle = document.getElementsByClassName("discussion-topic-title")[0].innerText;
+  var issueTitle = document.getElementsByClassName("js-issue-title")[0].innerText;
   var issueLink = 'Issue link: ' + window.location.toString();
   var issueBody = document.getElementsByClassName("comment-body")[0].textContent
   var thingsURL = 'things:add?notes=' + encodeURIComponent(issueLink + issueBody) + '&title=' + encodeURIComponent(issueTitle);
 
-  var target = document.getElementsByClassName("comment-topic-actions")[0].getElementsByTagName("li")[0];
+  var target = document.getElementsByClassName("flex-table-item-primary")[0];
   var btn = document.createElement("a");
   var text = document.createTextNode("Add to Things");
 
@@ -18,5 +18,4 @@
   btn.style["margin-right"] = "10px";
 
   target.insertBefore(btn, target.childNodes[0]);
-
 })();
